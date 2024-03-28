323effe80f7dba8ff31c6c613749971e064f3a93
Muhammad Usama <162746442+MUsama111@users.noreply.github.com>
2024-03-28 23:09:34 +0500

    Add files via upload
    I have created a number gussing game. I hope you will enjoy it.

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1) generate a computer generated random number
+// 2) take user input for gussing number
+// 3) compare user input with computer generated number.
+
+const RandomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "PersonGuessedNumber",
+    type: "number",
+    message: "Please Guess the number between 1 to 10:",
+  },
+]);
+
+if(answer.PersonGuessedNumber == RandomNumber){
+    console.log("Congratulations! you are right");
+}
+else{
+    console.log("Sorry better luck next time");
+}
